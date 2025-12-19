bb4c40b667a711a7211be146f04fb249a8eaa920
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2025-12-18 19:52:13 -0600

    Objetos

---
+++ Persona/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Persona
+{
+    public string Nombre;
+    public int Edad;
+    public float Altura;
+    public string Sexo;
+    public float Peso;
+
+    public void Saludar()
+    {
+        Console.WriteLine($"Hola Soy {Nombre}, tengo la edad de {Edad}, mido {Altura} metros, soy de sexo {Sexo} y peso {Peso} Libras.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Persona alumno = new Persona();
+        alumno.Nombre = "Luis";
+        alumno.Edad = 25;
+        alumno.Altura = 1.70f;
+        alumno.Sexo = "Masculino";
+        alumno.Peso = 140.0f;
+
+        alumno.Saludar();
+    }
+}
---
+++ Vehiculo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class vehiculo
+{
+    public string namepropietario;
+    public string marca;
+    public int decada;
+    public string color;
+
+    public void datos()
+    {
+        Console.WriteLine($"Hola! Soy {namepropietario} dueño de el vehículo {marca} del {decada} de color {color}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        vehiculo vehiculo1 = new vehiculo();
+        vehiculo1.namepropietario = "Angel";
+        vehiculo1.marca = "Toyota";
+        vehiculo1.decada = 2020;
+        vehiculo1.color = "rojo";
+
+        vehiculo1.datos();
+
+        vehiculo vehiculo2 = new vehiculo();
+        vehiculo2.namepropietario = "Carlos";
+        vehiculo2.marca = "Mitsubishi";
+        vehiculo2.decada = 2018;
+        vehiculo2.color = "azul";
+
+        vehiculo2.datos();
+    }
+}
